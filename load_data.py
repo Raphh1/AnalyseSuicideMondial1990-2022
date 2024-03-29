6a0ab7472c229f59f3fd45e07b3f94715806eb2d
craphael <raphael.cau@ynov.com>
2024-03-29 15:38:59 -0700

    projet data

---
+++ load_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import sqlite3
+
+
+df = pd.read_csv("GlobalYouTubeStatistics.csv", encoding='latin1')
+conn = sqlite3.connect("youtube.db")
+df.to_sql('youtubers', conn, if_exists='replace', index=False)
+
+
+conn.commit()
+conn.close()
