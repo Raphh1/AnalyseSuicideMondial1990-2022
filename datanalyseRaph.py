142e5146883faaf4f969a147e9d2a09ba9655fc3
craphael <raphael.cau@ynov.com>
2024-04-11 14:13:44 -0700

    new stat

--- datanalyseRaph.py
+++ datanalyseRaph.py
@@ -47,18 +47,29 @@ start_date = 2015
 end_date = 2022
 time_filtered = chart[(chart['Year'] >= start_date) & (chart['Year'] <= end_date)]
 asia = time_filtered[time_filtered['RegionCode'] == 'AS']
-annual_suicide_counts = asia.groupby('Year')['SuicideCount'].sum()
+africa = time_filtered[time_filtered['RegionCode'] == 'AF']
+europe = time_filtered[time_filtered['RegionCode'] == 'EU']
+centralsouthamerica = time_filtered[time_filtered['RegionCode'] == 'CSA']
+northamerica = time_filtered[time_filtered['RegionCode'] == 'NAC']
+annual_suicide_counts_asia = asia.groupby('Year')['SuicideCount'].sum()
+annual_suicide_counts_africa = africa.groupby('Year')['SuicideCount'].sum()
+annual_suicide_counts_europe = europe.groupby('Year')['SuicideCount'].sum()
+annual_suicide_counts_northamerica = northamerica.groupby('Year')['SuicideCount'].sum()
+annual_suicide_counts_centralsouthamerica = centralsouthamerica.groupby('Year')['SuicideCount'].sum()
 plt.figure(figsize=(10, 6)) 
-plt.plot(annual_suicide_counts.index, annual_suicide_counts.values, marker='o', linestyle='-', color='b')  
-
-plt.title('taux de suicides annuel en Asie')
+plt.plot(annual_suicide_counts_asia.index, annual_suicide_counts_asia.values, marker='o', linestyle='-', color='b', label='Asia')  
+plt.plot(annual_suicide_counts_africa.index, annual_suicide_counts_africa.values, marker='o', linestyle='-', color='r', label='Africa')  
+plt.plot(annual_suicide_counts_europe.index, annual_suicide_counts_europe.values, marker='o', linestyle='-', color='g', label='Europe') 
+plt.plot(annual_suicide_counts_northamerica.index, annual_suicide_counts_northamerica.values, marker='o', linestyle='-', color='y', label='North America')
+plt.plot(annual_suicide_counts_centralsouthamerica.index, annual_suicide_counts_centralsouthamerica.values, marker='o', linestyle='-', color='purple', label='Central and South America')     
+plt.title('Taux de suicides annuels par région')
 plt.xlabel('Year')
 plt.ylabel('Suicide Count')
 plt.grid(True)  
 plt.xticks(rotation=45)  
 plt.tight_layout()  
+plt.legend()  
 
 plt.show()
-
 
 
