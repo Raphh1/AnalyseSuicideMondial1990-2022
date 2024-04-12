c6bf89ecd3de5ff9003e5fe601f77dc3d2618d27
LeSam0 <samy.durand@ynov.com>
2024-04-12 16:15:53 +0200

    test map

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import json
 
  
 st.write("""
@@ -58,6 +60,79 @@ plt.tight_layout()
 plt.legend()  
 st.pyplot(figur)
 
+st.title('Corrélation entre le taux de suicides et le taux d\'inflation en Amérique du Nord')
 
 
+region = st.selectbox("Choisissez une région", time_filtered['RegionCode'].unique())
+nac_data = df[df['RegionCode'] == region]
+suicide_by_year = nac_data.groupby('Year')['SuicideCount'].sum()
+inflation_by_year = nac_data.groupby('Year')['EmploymentPopulationRatio'].mean()
+fig, ax1 = plt.subplots(figsize=(10, 6))
+
+color = 'tab:red'
+ax1.set_xlabel('Année')
+ax1.set_ylabel('Taux de suicides', color=color)
+ax1.plot(suicide_by_year.index, suicide_by_year.values, marker='o', linestyle='-', color=color, label='Taux de suicides')
+ax1.tick_params(axis='y', labelcolor=color)
+ax1.legend(loc='upper left')
+
+ax2 = ax1.twinx()  
+color = 'tab:blue'
+ax2.set_ylabel('Taux d\'emploi', color=color)  
+ax2.plot(inflation_by_year.index, inflation_by_year.values, marker='s', linestyle='--', color=color, label='Taux d\'emploi')
+ax2.tick_params(axis='y', labelcolor=color)
+ax2.legend(loc='upper right')
+
+fig.tight_layout()  
+
+st.pyplot(fig)
+
+nac_data = df[df['RegionCode'] == region]
+suicide_by_year = nac_data.groupby('Year')['SuicideCount'].sum()
+inflation_by_year = nac_data.groupby('Year')['InflationRate'].mean()
+fig, ax1 = plt.subplots(figsize=(10, 6))
+
+color = 'tab:red'
+ax1.set_xlabel('Année')
+ax1.set_ylabel('Taux de suicides', color=color)
+ax1.plot(suicide_by_year.index, suicide_by_year.values, marker='o', linestyle='-', color=color, label='Taux de suicides')
+ax1.tick_params(axis='y', labelcolor=color)
+ax1.legend(loc='upper left')
+
+ax2 = ax1.twinx()  
+color = 'tab:blue'
+ax2.set_ylabel('Taux d\'inflation', color=color)  
+ax2.plot(inflation_by_year.index, inflation_by_year.values, marker='s', linestyle='--', color=color, label='Taux d\'inflation')
+ax2.tick_params(axis='y', labelcolor=color)
+ax2.legend(loc='upper right')
+
+fig.tight_layout()  
+
+st.pyplot(fig)
+
+with open("countries.geo.json") as response:
+    geo = json.load(response)
+
+fig = go.Figure(
+    go.Choroplethmapbox(
+        geojson=geo,
+        locations=df['CountryCode'],
+        featureidkey="id",
+        z=df.groupby(['CountryCode'])['SuicideCount'].sum(),
+        colorscale="sunsetdark",
+        # zmin=0,
+        # zmax=500000,
+        marker_opacity=0.5,
+        marker_line_width=0,
+    )
+)
+fig.update_layout(
+    mapbox_style="carto-positron",
+    mapbox_zoom=6.6,
+    mapbox_center={"lat": 46.8, "lon": 8.2},
+    width=800,
+    height=600,
+)
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+st.plotly_chart(fig)
 #RegionCode,RegionName,CountryCode,CountryName,Year,Sex,AgeGroup,Generation,SuicideCount,CauseSpecificDeathPercentage,DeathRatePer100K,Population,GDP,GDPPerCapita,GrossNationalIncome,GNIPerCapita,InflationRate,EmploymentPopulationRatio
